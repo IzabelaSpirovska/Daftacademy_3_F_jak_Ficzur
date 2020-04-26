5e771141597f6546b06f1f034684cad6b222bdbe
Izabela <55464259+IzabelaSpirovska@users.noreply.github.com>
2020-04-26 23:16:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from hashlib import sha256
+from starlette.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends, Response, status
+import secrets
+
+security = HTTPBasic()
+
+
+@app.post("/login")
+def get_current_user(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
+    session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
+    app.session_tokens.append(session_token)
+    response.set_cookie(key="session_token", value=session_token)
+    response.headers["Location"] = "/welcome"
+    response.status_code = status.HTTP_302_FOUND 
